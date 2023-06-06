a7a5055c44721f59e87e5d659a7c622268a48f40
cillb <cillian.boland.1998@gmail.com>
2023-06-06 17:20:55 +0100

    Updated the player classes - added computer player. Human and computer players both have boards, make guesses etc.

--- Battleship_Game.py
+++ Battleship_Game.py
@@ -1,159 +1,83 @@
 """
-This program is a command line version of battleship. It is played against the computer, with the 
-player making guesses to where the computer has placed it's ships. There is set number of turns 
-that the player has to find all of the battleships. A class which the player uses to interact with 
-the game is imported.
+This runs a command line version of battleship. A computer opponent is used, it only plays random guesses.
+The boards for both players are printed out, hits are marked with 'X' and misses are marked with 'O'.
+A class for a human player and a computer player is created from the imported classes in Battleship_Player.
 """
 from random import choice
-from Battleship_Player import HumanPlayer
+from Battleship_Player import *
 from time import sleep
 # the game class is created, containing the functions to create the board, place ships, 
 # take guesses, check for hits/misses, and check for win/loss
 class Battleship:
     def __init__(self):
-        self.board = [([" "] * 10) for i in range(10)]
         self.current_winner = None
-        self.available_positions = []
-        for m in range(len(self.board)):
-            for n in range(len(self.board)):
-                self.available_positions.append((m, n))
-        self.all_ship_positions = []
-        self.individual_ships = []
-        self.fill_ocean()
     # print the ship in the command line
-    def print_board(self):
+    def print_board(self, p2):
+        print(f"\n\n\t\t{p2.name.upper()} BOARD\n")
         print("\t  1   2   3   4   5   6   7   8   9   10")
-        for x, row in enumerate(self.board):
+        for x, row in enumerate(p2.board):
             print("     " + str(x+1) + "\t| " + " | ".join(row) + " |")
-    # find a direction a given ship length can be placed in from it's starting position
-    def arrange_ship(self, first, second, direction, length):
-        points = set()
-        if direction == "h":
-            for i in range(1, length):
-                x = first, second + i
-                if x not in self.available_positions:
-                    for j in range(1, length):
-                        x = first, second - j
-                        if x not in self.available_positions:
-                            return points
-                        points.add(x)
-                        if len(points) == (length - 1):
-                            return points
-                points.add(x)
-                if len(points) == (length - 1):
-                    return points
-        if direction == "v":
-            for i in range(1, length):
-                x = first + i, second
-                if x not in self.available_positions:
-                    for j in range(1, length):
-                        x = first - j, second
-                        if x not in self.available_positions:
-                            return points
-                        points.add(x)
-                        if len(points) == (length - 1):
-                            return points
-                points.add(x)
-                if len(points) == (length - 1):
-                    return points
-    # take a starting position for a ship, and find an orientation for it, if it is not possible, 
-    # a new position will be used until a valid layout for a ship can be found
-    def create_ship(self, length):
-        while True:
-            startposition = choice(self.available_positions)
-            orientation = choice(["h", "v"])
-            start_row, start_col = startposition[0], startposition[1]
-            shippoints = set()
-            ship = self.arrange_ship(start_row, start_col, orientation, length)
-            if len(ship) == (length - 1):
-                shippoints.add(startposition)
-                for x in ship:
-                    shippoints.add(x)
-                return shippoints
-            else:
-                orientation = "h" if orientation == "v" else "v"
-                ship = self.arrange_ship(start_row, start_col, orientation, length)
-                if len(ship) == (length - 1):
-                    shippoints.add(startposition)
-                    for x in ship:
-                        shippoints.add(x)
-                    return shippoints
-                else:
-                    break
-    # list the length of each ship being placed, create each ship and store the positions each ship covers
-    def make_ships(self):
-        ship_lengths = [5, 4, 4, 3, 2]
-        for e in ship_lengths:
-            ind_ship = []
-            ship = self.create_ship(e)
-            for y in ship:
-                self.all_ship_positions.append(y)
-                ind_ship.append(y)
-                self.available_positions.remove(y)
-            self.individual_ships.append(ind_ship)
-    # run the functions to place the ships on the board
-    def fill_ocean(self):
-        self.make_ships()
-        return True
     # checks for a ship being sunk
-    def sunk_battleship(self, guess):
-        for target in self.individual_ships:
+    def sunk_battleship(self, p2: Player, guess: tuple):
+        for target in p2.ships:
             if guess in target:
                 target.remove(guess)
             if len(target) == 0:
-                self.individual_ships.remove(target)
+                p2.ships.remove(target)
                 return True
         return False
     # check if the guess made is a hit, miss, and if it is a win
-    def make_guess(self, guess):
+    def take_guess(self, guess, p1: Player, p2: Player):
         guess = guess
-        if self.board[guess[0]][guess[1]] == " ":
-            p1, p2 = guess[0] + 1, guess[1] + 1
-            print(f"\nPlayer fires on {p1}, {p2}!")
+        if p2.board[guess[0]][guess[1]] == " ":
+            g1, g2 = guess[0] + 1, guess[1] + 1
+            print(f"\n{p1.name} fires on {g1}, {g2}!")
             sleep(0.5)
-            if guess in self.all_ship_positions:
-                self.board[guess[0]][guess[1]] = "X"
+            if guess in p2.ship_points:
+                p2.board[guess[0]][guess[1]] = "X"
                 print("\nHit!")
-                self.all_ship_positions.remove(guess)
-                sunk = self.sunk_battleship(guess)
+                p2.ship_points.remove(guess)
+                sunk = self.sunk_battleship(p2, guess)
                 if sunk is True:
                     print("\nYou have sunk a battleship!")
             else:
-                self.board[guess[0]][guess[1]] = "O"
+                p2.board[guess[0]][guess[1]] = "O"
                 print("\nMiss!")
-            if self.winner():
-                self.current_winner = "Player"
+            if self.winner(p2):
+                self.current_winner = p1.name
             return True
         return False
     # check for winner
-    def winner(self):
-        if len(self.all_ship_positions) == 0:
+    def winner(self, p2: Player):
+        if len(p2.ship_points) == 0:
             return True
         else: return False
 # create a function to run the game, parameters are the game class, player class, and to print the game
-def play(game, player, print_game=True):
-    if print_game:# print the board, set the turns
-        game.print_board()
-        turn = 67# the actual number of turns will be turn - 1
-        sec = 0.5
+def play(game: Battleship, player: Player, comp: Player):
+    sec = 0.8
+    print("Let's Play Battleship!\nYou are playing against the computer.\n")
+    player.place_ships()
+    comp.place_ships()
+    game.print_board(player)
+    game.print_board(comp)
+    sleep(sec)
+    current_player, opp = player, comp
     while True:
-        if turn > 0:# run the game until there is a win or no turns left
-            turn -= 1
-            print(f"\nYou have {turn} shots remaining...")
-            sleep(sec)
-            guess = player.get_guess()
-            sleep(sec)
-            if game.make_guess(guess):
-                if print_game:
-                    sleep(sec)
-                    game.print_board()
-                    print("")
-                    sleep(sec)
-                if game.current_winner:
-                    return print("\nPlayer has sunk all of the ships! Player Wins!")
-        else: return print("\nOut of turns... Better luck next time!")
+        print("\n-----------------------------------------------------------")
+        print(f"\n{current_player.name} Turn.")
+        sleep(sec)
+        guess = current_player.make_guess()
+        sleep(sec)
+        game.take_guess(guess=guess, p1=current_player, p2=opp)
+        sleep(sec)
+        game.print_board(opp)
+        sleep(sec)
+        if game.current_winner:
+            return print(f"\n{game.current_winner} Wins!")
+        current_player, opp = opp, current_player
 # assign the player and game class, and input parameters to the play function
 if __name__ == "__main__":
     player = HumanPlayer()
-    battle = Battleship()
-    play(battle, player, print_game=True)
+    comp = ComputerPlayer()
+    game = Battleship()
+    play(game=game, player=player, comp=comp)
--- Battleship_Player.py
+++ Battleship_Player.py
@@ -4,19 +4,94 @@ The generic class initialises a set of guesses made by the player which is used 
 repeated guesses. The guess function takes input from the user, and checks if it is a valid guess
 by making sure integer values are entered that fall inside of a valid range, and that it is not a
 repeated guess.
+This program contains a base player class, which is inherited by human player and computer player classes.
+The human player class takes input to choose placements for the ships, and to make guesses against the computer.
+The inputs are validated and the player is notified of errors. The computer player only makes valid guesses and 
+is written to only successfully place valid ships on it's board.
 """
+from random import choice
+# the base class for a player, attributes are the name, lists to store ship positions and check if a ship is hit, 
+# and when a ship is sunk, the player's board that is printed during the game, and points on the board that are not covered by ships
 class Player:
     def __init__(self):
-        self.guesses = set()
-
-    def get_guess(self):
+        self.name = ""
+        self.ship_points = []
+        self.ships = []
+        self.board = [([" "] * 10) for i in range(10)]
+        self.available_points = []
+        for i in range(len(self.board)):
+            for j in range(len(self.board[0])):
+                self.available_points.append((i,j))
+    # the two base functions for a player to perform is to place ships at the start of the game, and make guesses during
+    def place_ships(self):
         pass
 
+    def make_guess(self):
+        pass
+# the human player sets the name attribute, and adds a new attribute to remember if a guess had already been made
 class HumanPlayer(Player):
     def __init__(self):
         super().__init__()
-
-    def get_guess(self):
+        self.name = "Player"
+        self.guesses_made = set()
+    # the player is prompted to choose a start point for the ships, and whether to place it across or down the board, the input has to be validated to ensure no errors
+    def place_ships(self):
+        new_board = [([" "] * 10) for i in range(10)]
+        def print_ships(new_board=new_board):# print the board with each new addition so the player knows where a ship can be placed
+            print("\t  1   2   3   4   5   6   7   8   9   10")
+            for x, row in enumerate(new_board):
+                print("     " + str(x+1) + "\t| " + " | ".join(row) + " |")
+        ship_lengths = [5, 4, 4, 3, 2]
+        Direction_Error = ValueError("Please enter either 'd' or 'a'.")
+        RangeError = ValueError("Please enter a position on the board.")
+        NotAvailableError = ValueError("Please place ship on available points.")
+        for length in ship_lengths:
+            ship = []
+            valid_placement = False
+            while not valid_placement:
+                print(f"\nPlacing ship, {length} spaces long.")
+                startrow, startcol = input("Enter the starting row (1-10):\t\t"), input("Enter the starting column (1-10):\t")
+                try:
+                    direction = input("Place ship down or across? (d/a)\t").lower()
+                    if direction not in ["d", "a"]:
+                        raise Direction_Error
+                    startpoint = int(startrow) - 1, int(startcol) - 1
+                    if direction == "d":
+                        for i in range(length):
+                            if (startpoint[0]+i,startpoint[1]) not in self.available_points:
+                                raise NotAvailableError
+                    else:
+                        for i in range(length):
+                            if (startpoint[0],startpoint[1]+i) not in self.available_points:
+                                raise NotAvailableError
+                    for point in startpoint:
+                        if point < 0 or point > 9:
+                            raise RangeError
+                    valid_placement = True
+                except ValueError as ve:
+                    if ve is RangeError:
+                        print(f"{str(ve)}")
+                    elif ve is Direction_Error:
+                        print(f"{str(ve)}")
+                    elif ve is NotAvailableError:
+                        print(f"{str(ve)}")
+                    else: print("Invalid placement, please try again.")
+            if direction == "d":
+                for point in [(startpoint[0]+i, startpoint[1]) for i in range(length)]:
+                    ship.append(point)
+                    self.available_points.remove(point)
+                    new_board[point[0]][point[1]] = "X"
+            else:
+                for point in [(startpoint[0],startpoint[1]+i) for i in range(length)]:
+                    ship.append(point)
+                    self.available_points.remove(point)
+                    new_board[point[0]][point[1]] = "X"
+            self.ships.append(ship)
+            self.ship_points.extend(ship)
+            print_ships()
+        print("\n-----------------------------------------------------------\n")
+    # take input from the player to fire on the opponents board, the input is validate to ensure it is a legitimate move
+    def make_guess(self):
         valid_guess = False
         range_error = ValueError("Please enter a postion on the board.")
         already_made_error = ValueError("You have already made that guess. Try again...")
@@ -28,9 +103,9 @@ class HumanPlayer(Player):
                 for num in guess:
                     if num < 0 or num > 9:
                         raise range_error
-                if guess in self.guesses:
+                if guess in self.guesses_made:
                     raise already_made_error
-                self.guesses.add(guess)
+                self.guesses_made.add(guess)
                 valid_guess = True
             except ValueError as ve:
                 if ve is range_error:
@@ -38,4 +113,81 @@ class HumanPlayer(Player):
                 elif ve is already_made_error:
                     print(f"\n{str(ve)}")
                 else: print("\nThat is an invalid input, please try again.")
+        return guess
+# the computer player sets the name, and adds a copy of the available moves which will be used to make guesses against the player
+class ComputerPlayer(Player):
+    def __init__(self):
+        super().__init__()
+        self.name = "Comp"
+        self.guesses_left = self.available_points
+    # the functions originally created in the game class are moved to here, the create_ship function tries to create a ship randomly until a valid placement is chosen
+    # when the arrange_ship function returns a ship of the desired length, the points the ship covers are added to the relevant lists
+    def place_ships(self):
+        ship_lengths = [5, 4, 4, 3, 2]
+        for e in ship_lengths:
+            ind_ship = []
+            ship = self.create_ship(e)
+            for y in ship:
+                self.ship_points.append(y)
+                ind_ship.append(y)
+                self.available_points.remove(y)
+            self.ships.append(ind_ship)
+    # given the start point and direction, return the points of the ship up to the desired length if valid, else less will be returned
+    def arrange_ship(self, first, second, direction, length):
+        points = set()
+        if direction == "h":
+            for i in range(1, length):
+                x = first, second + i
+                if x not in self.available_points:
+                    for j in range(1, length):
+                        x = first, second - j
+                        if x not in self.available_points:
+                            return points
+                        points.add(x)
+                        if len(points) == (length - 1):
+                            return points
+                points.add(x)
+                if len(points) == (length - 1):
+                    return points
+        if direction == "v":
+            for i in range(1, length):
+                x = first + i, second
+                if x not in self.available_points:
+                    for j in range(1, length):
+                        x = first - j, second
+                        if x not in self.available_points:
+                            return points
+                        points.add(x)
+                        if len(points) == (length - 1):
+                            return points
+                points.add(x)
+                if len(points) == (length - 1):
+                    return points
+    # try to create a random ship, when arrange_ship returns a ship with a valid length, it is stored in the ships and ship_points attributes
+    def create_ship(self, length):
+        while True:
+            startposition = choice(self.available_points)
+            orientation = choice(["h", "v"])
+            start_row, start_col = startposition[0], startposition[1]
+            shippoints = set()
+            ship = self.arrange_ship(start_row, start_col, orientation, length)
+            if len(ship) == (length - 1):
+                shippoints.add(startposition)
+                for x in ship:
+                    shippoints.add(x)
+                return shippoints
+            else:
+                orientation = "h" if orientation == "v" else "v"
+                ship = self.arrange_ship(start_row, start_col, orientation, length)
+                if len(ship) == (length - 1):
+                    shippoints.add(startposition)
+                    for x in ship:
+                        shippoints.add(x)
+                    return shippoints
+                else:
+                    break
+    # a random point in guesses_left is chosen and returned as the computer guess, it is also removed from the list, for now the computer does not focus on a hit point
+    def make_guess(self):
+        guess = choice(self.guesses_left)
+        self.guesses_left.remove(guess)
         return guess
